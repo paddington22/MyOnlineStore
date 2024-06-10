c8a8ab13fe9bdf2b350e2301de57561c1123d581
paddington22 <pkorolev23@gmail.com>
2024-06-10 21:08:04 +0300

    feat: added database settings

--- myOnlineStore/settings.py
+++ myOnlineStore/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'myOnlineStore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgres_psycopg2',
+        'NAME': 'myOnlineStore',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 

f4eb613570cbca8f8af476da73100c270a8d94c5
paddington22 <pkorolev23@gmail.com>
2024-06-11 14:06:25 +0300

    feat: created order status model

--- orders/admin.py
+++ orders/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from orders.models import Product, Category, Client
+from orders.models import Product, Category, Client, OrderStatus
 
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(Category)
 admin.site.register(Client)
+admin.site.register(OrderStatus)
+
--- orders/models.py
+++ orders/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 from django.db import models
 
+
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=100)
@@ -13,6 +14,7 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
 
 class Category(models.Model):
    name = models.CharField(max_length=255)
@@ -32,3 +34,10 @@ class Client(models.Model):
     def __str__(self):
         return self.first_name + ' ' + self.last_name
 
+
+class OrderStatus(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name

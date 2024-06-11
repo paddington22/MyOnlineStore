2f157d6fda4f1772508cfc2e09f818e2c86bd336
paddington22 <pkorolev23@gmail.com>
2024-06-11 15:23:37 +0300

    feat: created order model and added get_default_pk method to ordes status model

--- orders/admin.py
+++ orders/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
-from orders.models import Product, Category, Client, OrderStatus
+from orders.models import Product, Category, Client, OrderStatus, Order
 
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(Category)
 admin.site.register(Client)
 admin.site.register(OrderStatus)
+admin.site.register(Order)
 
--- orders/models.py
+++ orders/models.py
@@ -1,6 +1,5 @@
-from django.db import models
+from datetime import datetime
 
-# Create your models here.
 from django.db import models
 
 
@@ -32,12 +31,30 @@ class Client(models.Model):
     email = models.EmailField(null=True, blank=True)
 
     def __str__(self):
-        return self.first_name + ' ' + self.last_name
+        return f'{self.first_name} {self.last_name}'
 
 
 class OrderStatus(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
 
+    @classmethod
+    def get_default_pk(cls):
+        order_status, created = cls.objects.get_or_create(
+            name='В обработке',
+            defaults=dict(description='Ожидайте подтверждения'),
+        )
+        return order_status.pk
+
     def __str__(self):
         return self.name
+
+
+class Order(models.Model):
+    client_id = models.ForeignKey('Client', on_delete=models.CASCADE)
+    order_status_id = models.ForeignKey(to='OrderStatus', on_delete=models.PROTECT, default=OrderStatus.get_default_pk)
+    summary = models.DecimalField(max_digits=9, decimal_places=2)
+    order_datetime = models.DateTimeField(default=datetime.now())
+
+    def __str__(self):
+        return f'Заказ № {self.id} (заказчик: {self.client_id})'

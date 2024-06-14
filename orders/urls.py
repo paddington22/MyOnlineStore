2e17fc8ce742b1c28cb6becce1e1945aa360ec11
paddington22 <pkorolev23@gmail.com>
2024-06-14 17:55:09 +0300

    feat: rewrited product detail function as class

--- orders/urls.py
+++ orders/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import product_detail, ProductListView
+from .views import ProductListView, ProductDetailListView
 
 urlpatterns = [
     path('products/', ProductListView.as_view(), name='product-list'),
-    path('products/<int:pk>/', product_detail, name='product-detail'),
+    path('products/<int:pk>/', ProductDetailListView.as_view(), name='product-detail'),
     #path('add_to_cart/', add_to_cart, name='add-to-cart'),
 ]
--- orders/views.py
+++ orders/views.py
@@ -2,22 +2,31 @@ from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView
 from .models import Product
-
+from django.urls import reverse_lazy
 
 # Create your views here.
-class ProductListView(ListView):
+class ProductListView(LoginRequiredMixin, ListView):
+    #login_url = reverse_lazy('account_login')
     template_name = 'products/product_list.html'
 
     def get_queryset(self):
-        return Product.objects.all()
+        return Product.objects.all().order_by('name')
 
 
-#class ProductDetailView(ListView):
-#    template_name = 'products/product_detail.html'
+class ProductDetailListView(ListView):
+    template_name = 'products/product_detail.html'
 
+    def get_queryset(self):
+        return Product.objects.all().filter(pk=self.kwargs['pk'])
 
-def product_detail(request, pk):
-    product = Product.objects.get(pk=pk)
-    context = {'product': product, 'image': f'/{product.image}'}
-    return render(request, 'products/product_detail.html', context)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        product = Product.objects.get(pk=self.kwargs['pk'])
+        context['product'] = product
+        return context
+
+#def product_detail(request, pk):
+ #   product = Product.objects.get(pk=pk)
+  #  context = {'product': product, 'image': f'/{product.image}'}
+   # return render(request, 'products/product_detail.html', context)
 

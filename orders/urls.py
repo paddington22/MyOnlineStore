c1bc5d32789c907139719913d5b88c3e41714380
paddington22 <pkorolev23@gmail.com>
2024-06-14 14:38:59 +0300

    feat: rewrited product list function as class

--- orders/urls.py
+++ orders/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import product_list, product_detail
-
+from .views import product_detail, ProductListView
 
 urlpatterns = [
-    path('products/', product_list, name='product-list'),
+    path('products/', ProductListView.as_view(), name='product-list'),
     path('products/<int:pk>/', product_detail, name='product-detail'),
     #path('add_to_cart/', add_to_cart, name='add-to-cart'),
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView
 from .models import Product
 
+
 # Create your views here.
-def product_list(request):
-    products = Product.objects.all()
-    context = {'products': products}
-    return render(request, 'products/product_list.html', context)
+class ProductListView(ListView):
+    template_name = 'products/product_list.html'
+
+    def get_queryset(self):
+        return Product.objects.all()
+
+
+#class ProductDetailView(ListView):
+#    template_name = 'products/product_detail.html'
 
 
 def product_detail(request, pk):

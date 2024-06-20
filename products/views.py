9bb868c98b9683379db46272ea574cadf3ef9039
paddington22 <pkorolev23@gmail.com>
2024-06-20 16:18:23 +0300

    feat: added update func in product detail list if product in basket

--- products/views.py
+++ products/views.py
@@ -29,6 +29,14 @@ class ProductInBasketCreateView(LoginRequiredMixin, CreateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         product = Product.objects.get(pk=self.kwargs['pk'])
+        query = ProductInBasket.objects.filter(user_id=self.request.user.id).filter(product_id=product.id)
+
+        if not query.exists():
+            context['product_in_basket_id'] = query
+        else:
+            product_in_basket_id = ProductInBasket.objects.filter(user_id=self.request.user.id).get(product_id=product.id).id
+            context['product_in_basket_id'] = product_in_basket_id
+
         context['product'] = product
         return context
 

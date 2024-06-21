cfbb1e39b8b67b22aee788e7b823e144ba331212
paddington22 <pkorolev23@gmail.com>
2024-06-21 15:27:56 +0300

    feat: added logic of migrations product in basket to product in order

--- orders/views.py
+++ orders/views.py
@@ -6,31 +6,60 @@ from django.urls import reverse_lazy, reverse
 from .forms import OrderCreateForm
 from products.models import Product
 from users.models import ProductInBasket
+from .models import ProductInOrder
 
 
 # Create your views here.
 class OrderCreateView(LoginRequiredMixin, CreateView):
     template_name = 'orders/order_confirm.html'
     form_class = OrderCreateForm
+    success_url = reverse_lazy('profile')
+
+    def get_summary(self):
+        summary = 0
+        products_in_basket = self.get_queryset()
+
+        for product_in_basket in products_in_basket:
+            product = Product.objects.get(pk=product_in_basket.product_id)
+            temp_summary = product.unit_price * product_in_basket.quantity
+            summary += temp_summary
+
+        return summary
 
     def get_queryset(self):
         return ProductInBasket.objects.filter(user_id=self.request.user.id)
 
     def get_context_data(self, **kwargs):
-        summary = 0
         context = super().get_context_data(**kwargs)
-        products_in_basket = ProductInBasket.objects.filter(user_id=self.request.user.id)
+        products_in_basket = self.get_queryset()
         result = []
 
         for product_in_basket in products_in_basket:
             product = Product.objects.get(pk=product_in_basket.product_id)
-            temp_summary = product.unit_price * product_in_basket.quantity
             result.append([product_in_basket, product.unit_price, product.image])
-            summary += temp_summary
 
         context['products_in_basket_info'] = result
-        context['summary'] = summary
+        context['summary'] = self.get_summary()
         return context
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.summary = self.get_summary()
+        self.object.client_id = self.request.user.id
+        self.object.save()
+
+        products_in_basket = self.get_queryset()
+        for product_in_basket in products_in_basket:
+            ProductInOrder.objects.create(unit_price=Product.objects.get(id=product_in_basket.product_id).unit_price,
+                                          quantity=product_in_basket.quantity,
+                                          order_id=self.object.id,
+                                          product_id=product_in_basket.product_id)
+            ProductInBasket.objects.filter(id=product_in_basket.id).delete()
+
+        return super(OrderCreateView, self).form_valid(form)
+
+
+
 
 
 
